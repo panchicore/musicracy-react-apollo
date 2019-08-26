7288f68a9fd2b7e862f336ea30a97fc1392c6a86
panchicore <loopez>
2019-08-26 17:41:54 -0500

    layout changes on join screen

--- src/components/Join/index.js
+++ src/components/Join/index.js
@@ -1,12 +1,14 @@
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
 import {withRouter} from "react-router"
 import {Box, Button, Container, TextField, Typography} from "@material-ui/core"
 import {CONNECT_RADIO_MUTATION, USER_QUERY} from "../../graphql"
 import {Mutation, Query} from "react-apollo"
 import {CURRENT_RADIO_ID} from "../../constants"
+import {useSnackbar} from "notistack"
 
-function Join({history}){
+function Join({history, match}){
 
+  const { enqueueSnackbar } = useSnackbar()
   const [hash, setHash] = useState('')
   const [error, setError] = useState(null)
 
@@ -15,31 +17,42 @@ function Join({history}){
     history.push('/radio')
   }
 
+  useEffect(() => {
+    if(match.params.radioId){
+      setHash(match.params.radioId)
+    }
+  }, [])
+
+  useEffect(() => {
+    if(error){
+      error.graphQLErrors.map(err => enqueueSnackbar(err.message, {variant: 'error'}))
+
+    }
+  }, [error])
+
   return(
     <Container maxWidth="sm">
 
       <Query query={USER_QUERY}>
         {({data: {user}}) => {
           return (
-            <Box>
-              <Typography>Hi {user && user.name}!</Typography>
+            <Box display="flex" justifyContent="center" m={3}>
+              <Typography variant={'h3'}>Hi {user && user.name}!</Typography>
             </Box>
           )
         }}
       </Query>
 
-      <Box>
-        {error && <Typography>{error.message}</Typography>}
-      </Box>
-
       <Box m={1}>
         <TextField
           id="outlined-name"
-          label="Name"
+          label="Radio"
           value={hash}
           onChange={event => setHash(event.target.value)}
           margin="normal"
           variant="outlined"
+          fullWidth
+          required
         />
       </Box>
 
@@ -48,18 +61,28 @@ function Join({history}){
           mutation={CONNECT_RADIO_MUTATION}
           variables={{hash: hash}}
           onCompleted={onRadioConnect}
-          onError={(error) => setError(error.message)} >
+          onError={(error) => setError(error)} >
           {(connectToRadioMutation, {loading, error, data}) => (
-            <Button variant="contained" onClick={connectToRadioMutation}>
-              {loading ? 'Connecting...': 'Enter'}
+            <Button variant="contained" color="primary" onClick={connectToRadioMutation} fullWidth>
+              {loading ? 'Connecting...': 'Join now'}
             </Button>
           )}
         </Mutation>
       </Box>
 
       <Box m={1}>
-        <Button variant={"outlined"} onClick={() => history.push('/start')}>
+        <Button variant={"outlined"} onClick={() => history.push('/start')} fullWidth>
           Or create a radio
+        </Button>
+      </Box>
+
+      <Box display="flex" justifyContent="center" m={3}>
+        <Typography variant={'h3'}>Recover your music</Typography>
+      </Box>
+
+      <Box>
+        <Button variant="contained" color="primary" onClick={() => {}} fullWidth>
+          Login
         </Button>
       </Box>
 
